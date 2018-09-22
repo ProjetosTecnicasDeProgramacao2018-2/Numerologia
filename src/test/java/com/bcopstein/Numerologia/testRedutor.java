d6de30b0803b8f8afb37ab86b08e17625da4312a
dougl <dougl@192.168.15.50>
2018-09-22 20:59:05 -0300

    tirei o runnerJunit

--- src/test/java/com/bcopstein/Numerologia/testRedutor.java
+++ src/test/java/com/bcopstein/Numerologia/testRedutor.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.runner.RunWith;
 
-import junitparams.JUnitParamsRunner;
-//kkkeaeman
-@RunWith(JUnitParamsRunner.class)
+
 public class testRedutor {
 	private Redutor r=new Redutor();
 	@ParameterizedTest
